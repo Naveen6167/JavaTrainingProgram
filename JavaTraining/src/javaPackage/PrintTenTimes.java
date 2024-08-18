0fcca3c2b6c00fc44f983018067148bdd561733f
Naveen6167 <naveenanandan95@gmail.com>
2024-08-18 22:21:37 +0530

    commit print tentimes

---
+++ JavaTraining/src/javaPackage/PrintTenTimes.java
@@ -0,0 +1,17 @@
+package javaPackage;
+
+import java.util.Scanner;
+
+public class PrintTenTimes {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value"); //give 10
+		int a=sc.nextInt();
+				for (int i = 1; i <=a; i++)
+				System.out.println("Welcome to GUVI");
+			}
+			
+		}
+	
+	
