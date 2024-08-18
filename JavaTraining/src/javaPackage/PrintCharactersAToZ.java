039bea6b7cef74e90916d566047010f5f9676afb
Naveen6167 <naveenanandan95@gmail.com>
2024-08-18 21:30:12 +0530

    Print Char from A to Z

---
+++ JavaTraining/src/javaPackage/PrintCharactersAToZ.java
@@ -0,0 +1,13 @@
+package javaPackage;
+
+public class PrintCharactersAToZ {
+
+	public static void main(String[] args) {
+		System.out.println("The Characters are:");
+		for (char i = 'A'; i <= 'Z'; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
