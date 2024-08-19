abf80869f363d4b139a916a0b939671ee6701e70
Naveen6167 <naveenanandan95@gmail.com>
2024-08-19 20:38:18 +0530

    commit PrimeNumber

---
+++ JavaTraining/src/javaPackage/PrimeNumber.java
@@ -0,0 +1,31 @@
+package javaPackage;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int prime = sc.nextInt();
+		int count = 0;
+		if (prime > 1) {
+			for (int i = 1; i <= prime; i++) {
+
+				if (prime % i == 0)
+					count++;
+
+			}
+			if (count == 2) {
+				System.out.println("Prime Number");
+
+			} else {
+				System.out.println("Not a Prime");
+
+			}
+		} else {
+			System.out.println("Not a prime");
+		}
+	}
+
+}
