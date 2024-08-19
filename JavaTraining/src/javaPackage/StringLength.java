f6ab8c1b114a0b6eb4da0486a8cc9e93f22210f3
Naveen6167 <naveenanandan95@gmail.com>
2024-08-19 21:19:41 +0530

    commit StringLength

---
+++ JavaTraining/src/javaPackage/StringLength.java
@@ -0,0 +1,11 @@
+package javaPackage;
+
+public class StringLength {
+
+	public static void main(String[] args) {
+		String str="Guvi Geek";
+		System.out.println("Length of the String is:" +str.length());
+
+	}
+
+}
